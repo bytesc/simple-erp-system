fabaf5592382b864a01b9ffa7b7ecd0dcfe89637
rcy <21010078@mail.ecust.edu.cn>
2023-10-24 21:31:49 +0800

    treebuild

--- ERP/Tree.py
+++ ERP/Tree.py
@@ -5,7 +5,7 @@ import math
 from connectdb import select_from_db, exec_sql
 
 
-class Node:
+class ComposeItem:
     def __init__(self, father, child, way, comp_num,
                  loss_rate, store_1, store_2, adv_work, adv_make, adv_supply):
         self.father = father
@@ -18,8 +18,16 @@ class Node:
         self.adv_work = adv_work  # 作业提前期
         self.adv_make = adv_make  # 配料提前期
         self.adv_supply = adv_supply  # 供应商提前期
+
+
+class Node:
+    def __init__(self, compose_item: ComposeItem):
+        self.pname = compose_item.pname
+        self.father = compose_item.father
         self.depth = -1  # 节点深度
         self.child_depth = -1  # 子树的深度
+
+        self.children = []
 
 
 class ComposeTree:
@@ -28,7 +36,31 @@ class ComposeTree:
         self.MpsList = MpsList
         self.ans = ans
         self.compose = []
-        self.root = None
+
+    def build_tree(self, root: Node, depth):
+        root.depth = depth
+        for item in self.compose:
+            if item.father == root.pname:
+                root.children.append(Node(item))
+        if len(root.children) == 0:
+            return
+        for node in root.children:
+            self.build_tree(node, depth + 1)
+
+    def mark_child_depth(self, root: Node):  # 标记所有节点子树最大深度
+        if len(root.children) == 0:
+            root.child_depth = root.depth
+            return
+        for child in root.children:
+            self.mark_child_depth(child)
+        for child in root.children:
+            root.child_depth = max(root.child_depth, child.child_depth)
+
+    def refresh_store(self, item: Node, store_1, store_2):  # 刷新库存
+        for i in self.compose:
+            if i.pname == item.pname:
+                i.store_1 -= store_1
+                i.store_2 -= store_2
 
     async def refresh_db(self):
         for item in self.compose:
@@ -43,6 +75,42 @@ class ComposeTree:
             # print(sql_statement)
             await exec_sql(sql_statement)
         return
+
+    def main_dfs(self, node: Node, need_num, ans, end_time):
+        item = None
+        for i in self.compose:
+            if i.pname == node.pname and i.father == node.father:
+                item = i
+        if need_num <= 0:
+            return
+        need_num = math.ceil(need_num / (1 - item.loss_rate))  # 损耗
+        if need_num <= item.store_1 + item.store_2:  # 库存够
+            if need_num <= item.store_1:  # 工序够用
+                start_time = end_time - datetime.timedelta(days=item.adv_supply)
+                ans.append([item.pname, 0, item.way, start_time, end_time])
+                real_need_num = 0
+                self.refresh_store(item, need_num, 0)
+            else:  # 工序不够，但加上资材库存够用
+                start_time = end_time - datetime.timedelta(days=item.adv_supply + item.adv_make)
+                ans.append([item.pname, need_num - item.store_1, item.way, start_time, end_time])
+                real_need_num = 0
+                self.refresh_store(item, item.store_1, need_num - item.store_1)
+        else:  # 库存不够（工序和资材库存加起来都不够用）
+            start_time = end_time - datetime.timedelta(days=item.adv_supply + item.adv_make + item.adv_work)
+            ans.append([item.pname, need_num - item.store_2 - item.store_1, item.way, start_time, end_time])
+            real_need_num = need_num - item.store_1 - item.store_2
+            self.refresh_store(item, item.store_1, item.store_2)
+
+        if len(node.children) == 0:
+            return
+        else:
+            node.children.sort(key=lambda x: -x.child_depth)  # 按子树深度倒序，先遍历深的
+            for child in node.children:
+                comp_num = 1
+                for i in self.compose:
+                    if i.pname == child.pname and i.father == child.father:
+                        comp_num = i.comp_num
+                self.main_dfs(child, real_need_num * comp_num, ans, start_time)
 
     async def show_result(self):
         self.compose.clear()
@@ -61,105 +129,14 @@ class ComposeTree:
             # print(sql_res)
 
             for i in sql_res:
-                self.compose.append(Node(*i))
-
-            def mark_depth(item, deep):  # 标记所有节点深度
-                if item.depth == -1:
-                    i2 = copy.deepcopy(item)  # 新建节点
-                    i2.depth = deep
-                    self.compose.append(i2)
-
-                raw_child_items = []
-                for child in self.compose:
-                    if child.father == item.pname:
-                        raw_child_items.append(child)
-
-                raw_child_items.sort(key=lambda x: -x.depth)  # 排序，先遍历有深度的节点
-
-                child_items = []
-                for item in raw_child_items:  # 过滤
-                    if item.depth == deep:  # 深度相同，加入
-                        child_items.append(item)
-                    if item.depth == -1:  # 深度==-1，如果队中没有，加入
-                        find = 0
-                        for i2 in child_items:
-                            if i2.pname == item.pname:
-                                find = 1
-                        if find == 0:
-                            child_items.append(item)
-
-                if len(child_items) == 0:
-                    return
-                else:
-                    for child in child_items:
-                        mark_depth(child, deep + 1)
-
-            def mark_child_depth(item):  # 标记所有节点子树最大深度
-                child_items = []
-                for child in self.compose:
-                    if child.father == item.pname and child.depth == item.depth + 1 and child.depth != -1:
-                        child_items.append(child)
-                if len(child_items) == 0:
-                    item.child_depth = item.depth
-                    return
-                else:
-                    for child in child_items:
-                        mark_child_depth(child)
-                    for child in child_items:
-                        item.child_depth = max(item.child_depth, child.child_depth)
-
-            def refresh_store(item, store_1, store_2):  # 刷新库存
-                for i in self.compose:
-                    if i.pname == item.pname:
-                        i.store_1 -= store_1
-                        i.store_2 -= store_2
-
-            def main_dfs(item, need_num, ans, end_time):
-                if need_num <= 0:
-                    return
-                need_num = math.ceil(need_num / (1 - item.loss_rate))  # 损耗
-                real_need_num = need_num
-                if need_num <= item.store_1 + item.store_2:  # 库存够
-                    if need_num <= item.store_1:  # 工序够用
-                        start_time = end_time - datetime.timedelta(days=item.adv_supply)
-                        ans.append([item.pname, 0, item.way, start_time, end_time])
-                        real_need_num = 0
-                        refresh_store(item, need_num, 0)
-                    else:  # 工序不够，但加上资材库存够用
-                        start_time = end_time - datetime.timedelta(days=item.adv_supply + item.adv_make)
-                        ans.append([item.pname, need_num - item.store_1, item.way, start_time, end_time])
-                        real_need_num = 0
-                        refresh_store(item, item.store_1, need_num - item.store_1)
-                else:  # 库存不够（工序和资材库存加起来都不够用）
-                    start_time = end_time - datetime.timedelta(days=item.adv_supply + item.adv_make + item.adv_work)
-                    ans.append([item.pname, need_num - item.store_2 - item.store_1, item.way, start_time, end_time])
-                    real_need_num = need_num - item.store_1 - item.store_2
-                    refresh_store(item, item.store_1, item.store_2)
-
-                child_items = []
-                for child in self.compose:
-                    if child.father == item.pname and child.depth == item.depth + 1 and child.depth != -1:
-                        child_items.append(child)
-
-                if len(child_items) == 0:
-                    return
-                else:
-                    child_items.sort(key=lambda item: -item.child_depth)  # 按子树深度倒序，先遍历深的
-                    for child in child_items:
-                        main_dfs(child, real_need_num * child.comp_num, ans, start_time)
-
-            for item in self.compose:  # 找根节点计算深度
-                if item.father is None and item.depth == -1:
-                    mark_depth(item, 0)
-                    mark_child_depth(item)
+                self.compose.append(ComposeItem(*i))
 
             for mps in self.MpsList.MPS_obj_que:  # 遍历 mps 队列计算结果
-                treated = []  # 防止处理两遍
                 for item in self.compose:
-                    if mps.pname == item.pname and item.depth != -1:
-                        if item not in treated:
-                            main_dfs(item, mps.require, self.ans, mps.deadline)
-                            treated.append(item)
+                    if item.pname == mps.pname:
+                        root = Node(item)
+                        self.build_tree(root, 0)
+                        self.main_dfs(root, mps.require, self.ans, mps.deadline)
 
             await self.refresh_db()
         finally:
